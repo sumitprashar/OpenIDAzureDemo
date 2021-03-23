4a7e3e5d983a1b067e883eb186b997eb191cb9fb
Sumit Sharma <sumit.b.sharma@accenture.com>
2021-03-23 07:11:38 +0000

    OpenidConnect code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenIDAzureDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
